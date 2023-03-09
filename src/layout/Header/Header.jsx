20e1f9a25cdc253aeabe89232fdb8c2754610096
Andrei Beliakov <andrei@imd.ru>
2023-03-09 16:40:18 +0300

    feat: making header

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import s from "./Header.module.scss";
+import logo from "./images/logo.svg";
+import menu from "./images/menu.svg";
+import favorites from "./images/favorites.svg";
+import orders from "./images/orders.svg";
+import cart from "./images/cart.svg";
+import avatar from "./images/avatar.png";
+import { Button } from "../../ui/Button/Button";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className={s.header}>
+        <Button text="Каталог" />
+      </div>
+    </>
+  );
 };
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Header } from "../layout/Header/Header";
 
 export const Main = () => {
   return (
-    <div className={"container"}>
-      <h1>СЕВЕРЯНОЧКА</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
