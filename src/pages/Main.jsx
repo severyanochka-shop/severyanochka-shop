cf22e75326ef0743026760eff3a514ddda1e3f68
Igor Karpov <karpov_igorspb@mail.ru>
2023-03-09 18:17:54 +0300

    fix: select

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Button } from "../ui/Button/Button";
+import { Select } from "../ui/Select/Select";
 
 export const Main = () => {
+  const options = [
+    { value: "chocolate", label: "Chocolate11" },
+    { value: "strawberry", label: "Strawberry22" },
+    { value: "vanilla", label: "Vanilla33" },
+  ];
   return (
     <div className={"container"}>
       <h1>СЕВЕРЯНОЧКА</h1>
+      <Select options={options} />
     </div>
   );
 };
--- src/ui/Select/Select.jsx
+++ src/ui/Select/Select.jsx
@@ -1,36 +1,14 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import ReactSelect from "react-select";
 // import clsx from "clsx";
-// import s from "./Select.module.scss";
-
-// export const Select = ({ options, handler, className }) => {
-//   const [selectedOption, setSelectedOption] = useState([]);
-//   console.log(selectedOption);
-//   return (
-//     <ReactSelect
-//       isMulti
-//       defaultValue={["11111", "22222222", 333333333]}
-//       onChange={(e) => setSelectedOption([...selectedOption, e.target.value])}
-//       // className={clsx(s.select, className)}
-//       options={options}
-//     />
-//   );
-// };
-
-import React, { useState } from "react";
-
-const colourOptions = [
-  { value: "chocolate", label: "Chocolate" },
-  { value: "strawberry", label: "Strawberry" },
-  { value: "vanilla", label: "Vanilla" },
-];
+import s from "./Select.module.scss";
 
 const styles = {
   multiValue: (base, state) => {
-    return state.data.isFixed ? { ...base, backgroundColor: "#FF6633" } : base;
+    return !state.data.isFixed ? { ...base, backgroundColor: "#FF6633", color: "#FFFFFF" } : base;
   },
   multiValueLabel: (base, state) => {
-    return state.data.isFixed ? { ...base, fontWeight: "bold", color: "white", paddingRight: 6 } : base;
+    return !state.data.isFixed ? { ...base, color: "white", paddingRight: 6 } : base;
   },
   multiValueRemove: (base, state) => {
     return state.data.isFixed ? { ...base, display: "none" } : base;
@@ -41,7 +19,7 @@ const orderOptions = (values) => {
   return values.filter((v) => v.isFixed).concat(values.filter((v) => !v.isFixed));
 };
 
-export const Select = () => {
+export const Select = ({ options }) => {
   const [value, setValue] = useState([]);
 
   const onChange = (newValue, actionMeta) => {
@@ -53,24 +31,16 @@ export const Select = () => {
         }
         break;
       case "clear":
-        newValue = colourOptions.filter((v) => v.isFixed);
+        newValue = options.filter((v) => v.isFixed);
+        break;
+      default:
         break;
     }
 
     setValue(orderOptions(newValue));
   };
 
-  return (
-    <ReactSelect
-      value={value}
-      isMulti
-      styles={styles}
-      isClearable={value.some((v) => !v.isFixed)}
-      name="colors"
-      className="basic-multi-select"
-      classNamePrefix="select"
-      onChange={onChange}
-      options={colourOptions}
-    />
-  );
+  console.log(value);
+
+  return <ReactSelect value={value} isMulti styles={styles} onChange={onChange} options={options} />;
 };
