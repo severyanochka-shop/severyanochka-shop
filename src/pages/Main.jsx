3b26b2b2226ec06f411654ff0fcdf1a9cc4db133
OrangeRootkit <katkovdv88@gmail.com>
2023-03-10 14:26:15 +0300

    feat: add toggle 4sizes with all conditions

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,10 @@
 import React from "react";
-import { Button } from "../ui/Button/Button";
-import {Toggle} from "../ui/Toggle/Toggle";
+
 
 export const Main = () => {
   return (
     <div className={"container"}>
       <h1>СЕВЕРЯНОЧКА</h1>
-
-      {/* <Toggle size = {"toggle-switch__XL"}/> */}
-      {/* <Toggle size = {"toggle-switch__L"}/> */}
-            {/* <Toggle size={"XL"}/>
-            <Toggle size={"L"}/>   */}
-            <Toggle XL checked={false}/>
-            <Toggle L checked={false}/>  
     </div>
   );
 };
--- src/ui/Toggle/Toggle.jsx
+++ src/ui/Toggle/Toggle.jsx
@@ -2,91 +2,56 @@ import React from "react";
 import "./Toggle.scss";
 import clsx from "clsx";
 
-export const Toggle = ({ label, L, XL, checked, desable }) => {
-  console.log(checked);
+export const Toggle = ({ L, XL, M, S, checked, disable, handler }) => {
 
-  const innerClassName = clsx("toggle-switch", {
+  const innerClassNameToggle = clsx("toggle-switch", {
     "toggle-switch__XL": XL,
     "toggle-switch__L": L,
+    "toggle-switch__M": M,
+    "toggle-switch__S": S,
   });
 
-  // const innerClassNameInner = clsx(checked == false? "inner__noCheck": "inner__checked", {
-  //   "inner__XL": XL,
-  //   "inner__L": L,
-  // })
+  const innerClassNameInner = clsx(
+    checked === false
+      ? ("inner",
+        {
+          inner__XL: XL,
+          inner__L: L,
+          inner__M: M,
+          inner__S: S,
 
-  let innerClassNameInner = "";
-
-  if (checked == true) {
-    innerClassNameInner = clsx("inner__checked", {
-      inner__checked__XL: XL,
-      inner__checked__L: L,
-    });
-  } else {
-    innerClassNameInner = clsx("inner", {
-      inner__XL: XL,
-      inner__L: L,
-    });
-
-  }
+          inner__XL__disable: disable === true,
+        })
+      : ("inner-checked",
+        {
+          inner__checked__XL: XL,
+          inner__checked__L: L,
+          inner__checked__M: M,
+          inner__checked__S: S,
+          inner__checked__disable: disable === true,
+        })
+  );
 
   const innerClassNameSwitch = clsx(
     checked == false ? "switch" : "switch__checked",
     {
       switch__XL: XL,
       switch__L: L,
+      switch__M: M,
+      switch__S: S,
+
+      switch__disable: disable === true,
+      switch__checked__disable: disable === true,
     }
   );
 
-  // let innerClassNameSwitch = ''
-
-  // if (check===true)
-  //  innerClassNameSwitch = clsx("switch", {
-  //   "switch__XL": XL,
-  //   "switch__L": L,
-  // })
-
   return (
-    // <div className="container">
-    //   {label}{"agagsgsg"}
-
-    // <div className={"toggle-switch"}>
-    <div className={innerClassName}>
-      <input
-        checked={true}
-        type="checkbox"
-        className="checkbox"
-        name={label}
-        id={label}
-        onChange={() => {}}
-        // checked
-      />
-      <label className="label" htmlFor={label}>
+    <div className={innerClassNameToggle} onClick={() => handler()}>
+      <input type="checkbox" className="checkbox" />
+      <label className="label">
         <span className={innerClassNameInner} />
         <span className={innerClassNameSwitch} />
       </label>
     </div>
-    // </div>
   );
 };
-
-// import React from "react";
-// import "./Toggle.scss";
-
-// const ToggleSwitch = ({ label }) => {
-//   return (
-//     <div className="container">
-//       {label}{" "}
-//       <div className="toggle-switch">
-//         <input type="checkbox" className="checkbox"
-//             name={label} id={label} checked/>
-//         <label className="label" htmlFor={label}>
-//           <span className="inner" />
-//           <span className="switch" />
-//         </label>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ToggleSwitch;
