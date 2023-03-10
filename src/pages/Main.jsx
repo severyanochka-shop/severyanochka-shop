72df4dcce564d8c47fe04b876c3b778239645a12
Andrei Beliakov <andrei@imd.ru>
2023-03-10 18:08:44 +0300

    fix: main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { Header } from "../layout/Header/Header";
-import { Button } from "../ui/Button/Button";
 
 export const Main = () => {
   return (
     <div className={"container"}>
       <h1>СЕВЕРЯНОЧКА</h1>
-      <Header />
     </div>
   );
 };
