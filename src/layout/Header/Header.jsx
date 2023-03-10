861592f25b791cd8384f8ddedf90a1164b5a56e8
Andrei Beliakov <andrei@imd.ru>
2023-03-10 15:46:44 +0300

    feat:add PictoButton component

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -8,27 +8,39 @@ import favorites from "./images/favorites.svg";
 import orders from "./images/orders.svg";
 import cart from "./images/cart.svg";
 import avatar from "./images/avatar.png";
-import name from "./images/name.svg";
 import { HeaderButton } from "./HeaderButton/HeaderButton";
-
+import { PictoButton } from "./PictoButton/PictoButton";
 import { Container } from "../Container/Container";
 
 export const Header = ({ handler }) => {
   return (
     <div className={s.header}>
       <Container>
-        <Link to="/">
-          <img src={logo} alt="ЛОГО" className={s.img_logo} />
-          <img src={name} alt="Северяночка" className={s.img_name} />
-        </Link>
-        <Link to="/">
-          <HeaderButton handler background="green">
-            <div className={s.button__catalogue}>
-              <img src={menu} alt="|||" />
-              <p className={s.button__text}>Каталог</p>
-            </div>
-          </HeaderButton>
-        </Link>
+        <div className={s.nav}>
+          <Link to="/">
+            <img src={logo} alt="ЛОГО" className={s.logo} />
+          </Link>
+          <Link to="/catalogue">
+            <HeaderButton handler background="green">
+              <div className={s.button__catalogue}>
+                <img src={menu} alt="|||" />
+                <p className={s.button__text}>Каталог</p>
+              </div>
+            </HeaderButton>
+          </Link>
+          <input className={s.header_input} placeholder="Найти товар" />
+          <PictoButton text={"Избранное"} img={favorites} link="/favorites" />
+          <PictoButton text={"Заказы"} img={orders} link="/orders" />
+          <PictoButton text={"Корзина"} img={cart} link="/shopping-cart" />
+          <Link to="/login">
+            <HeaderButton handler background="orange">
+              <div className={s.button__catalogue}>
+                <p className={s.button__text}>Войти</p>
+                <img src={login} alt="->" />
+              </div>
+            </HeaderButton>
+          </Link>
+        </div>
       </Container>
     </div>
   );
---
+++ src/layout/Header/PictoButton/PictoButton.jsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import clsx from "clsx";
+import s from "./PictoButton.module.scss";
+
+export const PictoButton = ({ text, img, link }) => {
+  return (
+    <>
+      <Link to={link}>
+        <button className={s.button}>
+          <img src={img} alt="" />
+          {text}
+        </button>
+      </Link>{" "}
+    </>
+  );
+};
