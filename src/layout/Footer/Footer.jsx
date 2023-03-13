83f3aaf468592daa9a2722ef6024c014cc9cb1d0
I-D-D-I <3711080@gmail.com>
2023-03-13 15:30:59 +0500

    fix: footer

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -9,12 +9,28 @@ import zas4x from './images/zas4x.png'
 import logosev from './images/logosev.png'
 
 export const Footer = () => {
+  let data = [
+    "О компании",
+    "Контакты",
+    "Вакансии",
+    "Статьи",
+    "Политика обработки персональных данных",
+  ];
     return (
       <>
         <div className={s.footer}>
           <img src={logosev} className={s.footer__logo} alt="logo" />
-          <div className={s.footer__links}>
-            <a href="#" className={s.footer__link}>
+          {/* <div className={s.footer__links}> */}
+          <ul className={s.footer__links}>
+            {data.map((el, ind) => (
+              <li key={ind}>
+                <a href="#" className={s.footer__link}>
+                  {el}
+                </a>
+              </li>
+            ))}
+          </ul>
+          {/* <a href="#" className={s.footer__link}>
               О компании
             </a>
             <a className={s.footer__link} href="#">
@@ -28,31 +44,31 @@ export const Footer = () => {
             </a>
             <a className={s.footer__link} href="#">
               Политика обработки персональных данных
-            </a>
-          </div>
+            </a> */}
+          {/* </div> */}
           <div className={s.footer__items}>
             <a className={s.footer__item} href="">
-              <img src={Instagram} alt="logo" width="20" height="20" />
+              <img src={Instagram} alt="logo" />
             </a>
             <a className={s.footer__item} href="">
-              <img src={VK} alt="logo" width="20" height="20" />
+              <img src={VK} alt="logo" />
             </a>
             <a className={s.footer__item} href="">
-              <img src={Facebook} alt="logo" width="20" height="20" />
+              <img src={Facebook} alt="logo" />
             </a>
             <a className={s.footer__item} href="">
-              <img src={Ok} alt="logo" width="20" height="20" />
+              <img src={Ok} alt="logo" />
             </a>
           </div>
           <div className={s.footer__contact}>
             <a className={s.footer__tel} href="">
-              <img src={Tel} alt="logo" width="20" height="20" />
+              <img src={Tel} alt="logo" />
             </a>
             <p className={s.footer__numbers}>8 800 777 33 33</p>
           </div>
           <div className={s.footer__design}>
             <p className={s.footer__des}>Дизайн</p>
-            <img src={zas4x} alt="logo" width="100" height="10" />
+            <img src={zas4x} alt="logo" className={s.footer__deslogo} />
           </div>
         </div>
       </>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Footer } from "../layout/Footer/Footer";
 
 export const Main = () => {
   return (
     <div className={"container"}>
       <h1>СЕВЕРЯНОЧКА</h1>
-            
+
+      <Footer />
     </div>
   );
 };
