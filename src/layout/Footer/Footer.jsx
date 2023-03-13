c52c622a687f7772bd144ddc0d0d40e617780723
I-D-D-I <3711080@gmail.com>
2023-03-13 20:35:00 +0500

    fix: footer adaptive

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -5,8 +5,8 @@ import Instagram from "./images/Instagram.svg";
 import Ok from "./images/Ok.svg";
 import VK from "./images/VK.svg";
 import Tel from "./images/Tel.svg";
-import Zasov from './images/Zasov.svg'
-import logosev from './images/logosev.png'
+import Zasov from "./images/Zasov.svg";
+import logosev from "./images/logosev.png";
 
 export const Footer = () => {
   let data = [
@@ -16,43 +16,40 @@ export const Footer = () => {
     "Статьи",
     "Политика обработки персональных данных",
   ];
-  let src = [
-    Instagram,
-    VK,
-    Facebook,
-    Ok,
-  ];
-    return (
-      <>
-        <div className={s.footer}>
-          <img src={logosev} className={s.footer__logo} alt="logo" />
-          <ul className={s.footer__links}>
-            {data.map((el, ind) => (
-              <li key={ind}>
-                <a href="#" className={s.footer__link}>
-                  {el}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <div className={s.footer__items}>
-            {src.map((el, ind) => (
-              <a className={s.footer__item} key={ind}>
-              <img src={el} alt="logo" />
+  let src = [Instagram, VK, Facebook, Ok];
+  return (
+    <>
+      <div className={s.footer}>
+        <img src={logosev} className={s.footer__logo} alt="logo" />
+        {/* <div className={s.footer__center}> */}
+        <ul className={s.footer__links}>
+          {data.map((el, ind) => (
+            <li key={ind}>
+              <a href="#" className={s.footer__link}>
+                {el}
               </a>
-            ))}
-          </div>
-          <div className={s.footer__contact}>
-            <a className={s.footer__tel} href="">
-              <img src={Tel} alt="logo" />
+            </li>
+          ))}
+        </ul>
+        <div className={s.footer__items}>
+          {src.map((el, ind) => (
+            <a className={s.footer__item} key={ind}>
+              <img src={el} alt="logo" className={s.footer__img} />
             </a>
-            <p className={s.footer__numbers}>8 800 777 33 33</p>
-          </div>
-          <div className={s.footer__design}>
-            <p className={s.footer__des}>Дизайн</p>
-            <img src={Zasov} alt="logo" className={s.footer__deslogo} />
-          </div>
+          ))}
         </div>
-      </>
-    );
+        {/* </div> */}
+        <div className={s.footer__contact}>
+          <a className={s.footer__tel} href="">
+            <img src={Tel} alt="logo" />
+          </a>
+          <p className={s.footer__numbers}>8 800 777 33 33</p>
+        </div>
+        <div className={s.footer__design}>
+          <p className={s.footer__des}>Дизайн</p>
+          <img src={Zasov} alt="logo" className={s.footer__deslogo} />
+        </div>
+      </div>
+    </>
+  );
 };
