d180505430728e48e2b7178632438711bfcda6d4
Igor Karpov <karpov_igorspb@mail.ru>
2023-03-13 18:33:16 +0300

    refactor: Main

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Container } from "../layout/Container/Container";
-import { Tooltips } from "../ui/Tooltips/Tooltips";
 
 export const Main = () => {
   return (
     <Container>
       <h1>СЕВЕРЯНОЧКА</h1>
-      <Tooltips gray text="123" />
     </Container>
   );
 };
