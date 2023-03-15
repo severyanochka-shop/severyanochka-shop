3c0d2438e5dcd83f02718c9c2b2d07098d18ac8a
Andrei Beliakov <andrei@imd.ru>
2023-03-15 17:18:46 +0300

    fix:main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { Header } from "../layout/Header/Header";
 
 export const Main = () => {
   return (
-    <div className={"container"}>
+    <>
       <h1>СЕВЕРЯНОЧКА</h1>
-      <Header />
-    </div>
+    </>
   );
 };
