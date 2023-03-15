47862b5836235c288b63cbfa7b1a7d0daec9cd74
Margolex <margaritalexina@gmail.com>
2023-03-15 19:52:43 +0300

    feat: create element

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { SimilarProduct } from "../../components/SimilarProduct/SimilarProduct";
+
 import { Layout } from "../../layout/Layout";
+
 
 export const Main = () => {
   return (
