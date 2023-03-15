3a0dcc14e1bdaf92d15b28b47f53c7f0d16020bf
I-D-D-I <3711080@gmail.com>
2023-03-15 15:11:06 +0500

    fix: textfield search

--- src/ui/Textfield/Textfield.jsx
+++ src/ui/Textfield/Textfield.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import clsx from "clsx";
 import s from "./Textfield.module.scss";
 import eye from "./eye.svg";
+import search from "./search.svg";
 
 export const Textfield = ({ className, label, placeholder, type, disabled, medium, large }) => {
   return (
@@ -22,6 +23,11 @@ export const Textfield = ({ className, label, placeholder, type, disabled, mediu
         {type === "password" && (
           <a href="#" className={s.textfield__password}>
             <img src={eye} alt="eye" className="textfield__eye" />
+          </a>
+        )}
+        {placeholder === "Найти товар" && (
+          <a href="#" className={s.textfield__password}>
+            <img src={search} alt="search" className="textfield__eye" />
           </a>
         )}
       </label>
