c6a85a1ceedb45238854e3aab165fd2b1a1c9cf2
I-D-D-I <3711080@gmail.com>
2023-03-15 21:07:25 +0500

    feat: add AboutCompany

---
+++ src/components/AboutCompany/AboutCompany.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Container } from "../../layout/Container/Container.jsx";
+import s from "./AboutCompany.module.scss";
+import group7105 from "./images/group7105.svg";
+import groupPeople from "./images/groupPeople.svg";
+import сhecksev from "./images/checksev.svg";
+import logosevabout from "./images/logosevabout.svg";
+
+export const AboutCompany = () => {
+  return (
+    <div className={s.about}>
+      <Container>
+        <div className={s.abouttitle}>
+          {/* <img src={group7105} alt="groupPeople" className={s.abouttitle_img} /> */}
+
+          <div className={s.titles}>
+            <h1 className={s.title}>О компании</h1>
+            <h3 className={s.subtitle}>
+              Мы непрерывно развиваемся и
+              <br />
+              работаем над совершенствованием сервиса,
+              <br />
+              заботимся о наших клиентах,
+              <br />
+              стремимся к лучшему будущему.
+            </h3>
+          </div>
+          <img src={groupPeople} alt="groupPeople" className={s.about__img} />
+        </div>
+        <div className={s.checkboxes}>
+          <div className={s.texts}>
+            <img src={сhecksev} alt="сheck" className={s.check__img} />
+            <p className={s.textOne}>Мы занимаемся розничной торговлей</p>
+            <p className={s.textTwo}>Более 20 лет.</p>
+          </div>
+          <div className={s.texts}>
+            <img src={сhecksev} alt="сheck" className={s.check__img} />
+            <p className={s.textOne}>Основная миссия компании</p>
+            <p className={s.textTwo}>Максимальное качество товаров и услуг по доступной цене.</p>
+          </div>
+          <div className={s.texts}>
+            <img src={сhecksev} alt="сheck" className={s.check__img} />
+            <p className={s.textOne}>Отличительная черта нашей сети</p>
+            <p className={s.textTwo}>
+              Здоровая и полезная продукция местного производства в наших магазинах.
+            </p>
+          </div>
+        </div>
+        <div className={s.thanks}>
+          <img src={logosevabout} alt="logo" className={s.thanks__img} />
+          <p className={s.thanks__text}>Спасибо за то, что вы с нами. Северяночка, везет всегда!</p>
+        </div>
+      </Container>
+    </div>
+  );
+};
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { AboutCompany } from "../components/AboutCompany/AboutCompany";
 
 export const Main = () => {
   return (
     <>
       <h1>СЕВЕРЯНОЧКА</h1>
 
+      <AboutCompany />
     </>
   );
 };
