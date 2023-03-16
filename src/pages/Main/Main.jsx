8820a3f8ae837c5a1b2dbae4e9d8e59706d181dd
Andrei Beliakov <andrei@imd.ru>
2023-03-16 18:57:40 +0300

    feat: DeliveryFree banner

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { DeliveryFree } from "../../components/DeliveryFree/DeliveryFree";
 
 import { Layout } from "../../layout/Layout";
 
 export const Main = () => {
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>СЕВЕРЯНОЧКА</h1>
+      <DeliveryFree />
+      {/* <h1 style={{ textAlign: "center" }}>СЕВЕРЯНОЧКА</h1> */}
     </>
   );
 };
