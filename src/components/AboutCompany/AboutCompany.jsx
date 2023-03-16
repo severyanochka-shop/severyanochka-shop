86526682298fef618376d043045269668b72f709
I-D-D-I <3711080@gmail.com>
2023-03-16 18:09:29 +0500

    fix: AboutCompany

--- src/components/AboutCompany/AboutCompany.jsx
+++ src/components/AboutCompany/AboutCompany.jsx
@@ -5,6 +5,7 @@ import group7105 from "./images/group7105.svg";
 import groupPeople from "./images/groupPeople.svg";
 import сhecksev from "./images/checksev.svg";
 import logosevabout from "./images/logosevabout.svg";
+import rectangle from "./images/rectangle.svg";
 
 export const AboutCompany = () => {
   return (
@@ -19,7 +20,7 @@ export const AboutCompany = () => {
               Мы непрерывно развиваемся и
               <br />
               работаем над совершенствованием сервиса,
-              <br />
+              <br className={s.subtitle__br} />
               заботимся о наших клиентах,
               <br />
               стремимся к лучшему будущему.
@@ -33,12 +34,12 @@ export const AboutCompany = () => {
             <p className={s.textOne}>Мы занимаемся розничной торговлей</p>
             <p className={s.textTwo}>Более 20 лет.</p>
           </div>
-          <div className={s.texts}>
+          <div className={`${s.texts} ${s.textssecond}`}>
             <img src={сhecksev} alt="сheck" className={s.check__img} />
             <p className={s.textOne}>Основная миссия компании</p>
             <p className={s.textTwo}>Максимальное качество товаров и услуг по доступной цене.</p>
           </div>
-          <div className={s.texts}>
+          <div className={`${s.texts} ${s.textsthree}`}>
             <img src={сhecksev} alt="сheck" className={s.check__img} />
             <p className={s.textOne}>Отличительная черта нашей сети</p>
             <p className={s.textTwo}>
@@ -48,6 +49,9 @@ export const AboutCompany = () => {
         </div>
         <div className={s.thanks}>
           <img src={logosevabout} alt="logo" className={s.thanks__img} />
+          {/* <div className={s.thanks__triangle}>tri</div> */}
+          <img src={rectangle} alt="rectangle" className={s.thanks__triangle} />
+
           <p className={s.thanks__text}>Спасибо за то, что вы с нами. Северяночка, везет всегда!</p>
         </div>
       </Container>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { AboutCompany } from "../components/AboutCompany/AboutCompany";
 
 export const Main = () => {
   return (
     <>
       <h1>СЕВЕРЯНОЧКА</h1>
-
-      <AboutCompany />
     </>
   );
 };
