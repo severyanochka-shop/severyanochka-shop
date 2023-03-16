a4e9b50f5046546de4cd655cea2082f79a1f42cd
zimoscop <zimoscop@gmail.com>
2023-03-16 10:07:09 +0300

    fix:adaptiv

--- src/components/ContactPage/ContactPage.jsx
+++ src/components/ContactPage/ContactPage.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import s from "./ContactPage.module.scss";
 import map from "./Image/map.svg";
-import adopmap from "./Image/adopt.jpg";
+import minmap from "./Image/minmap.jpg";
 import voshod from "./Image/voshod.svg";
+import voshodonmap from "./Image/voshodonmap.svg";
 import parus from "./Image/parus.svg";
+import parusonmap from "./Image/parusonmap.svg";
 import rjaba from "./Image/rjabina.svg";
+import rjabaonmap from "./Image/rjabaonmap.svg";
 import los from "./Image/pellis.svg";
+import pelisonmap from "./Image/pelisonmap.svg";
 
 export const ContactPage = () => {
   return (
@@ -14,12 +18,20 @@ export const ContactPage = () => {
       <div className={s.contact}>
         <div className={s.item1}>
           <div className={s.box}>
-            <img className={s.box__img} src={require("./Image/home.png")} alt="home" />{" "}
-            <span className={s.box__text}>Бухгалтерия, склад</span>
+            <img
+              className={s.box__img}
+              src={require("./Image/home.png")}
+              alt="home"
+            />{" "}
+            <span className={(s.box__text, s.first)}>Бухгалтерия, склад</span>
             <p className={s.box__tel}>+7 82140 92619</p>
           </div>
           <div className={s.box}>
-            <img className={s.box__img} src={require("./Image/percent.png")} alt="%" />{" "}
+            <img
+              className={s.box__img}
+              src={require("./Image/percent.png")}
+              alt="%"
+            />{" "}
             <span className={s.box__text}>Вопросы по системе лояльности</span>
             <p className={s.box__tel}>+7 908 716 33 97</p>
           </div>
@@ -38,11 +50,15 @@ export const ContactPage = () => {
           <img src={los} alt="Пелысь" />
         </div>
         <div className={s.item4}>
-          <picture>
-            <source media="(max-width: 880px)" srcSet={adopmap} />
-            <source media="(min-width: 881px)" srcSet={map} />
+          <picture className={s.item4__picture}>
+            <source media="(max-width: 450px)" srcSet={minmap} />
+            <source media="(max-width: 1100px)" srcSet={map} />
             <img src={map} alt="Карта" />
           </picture>
+          <img className={s.item4__voshod} src={voshodonmap} alt="voshod" />
+          <img className={s.item4__parus} src={parusonmap} alt="parus" />
+          <img className={s.item4__rjaba} src={rjabaonmap} alt="parus" />
+          <img className={s.item4__pelis} src={pelisonmap} alt="parus" />
         </div>
       </div>
     </div>
