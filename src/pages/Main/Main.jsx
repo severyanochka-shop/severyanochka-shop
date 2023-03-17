7fcb2880f6981732b0761250cd2dac5e9d64317a
Andrei Beliakov <andrei@imd.ru>
2023-03-17 18:33:36 +0300

    feat: Articles

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { CardCatalog } from "../../components/CardCatalog/CardCatalog";
+import { Articles } from "../../components/Articles/Articles";
 
 import { Layout } from "../../layout/Layout";
 
 export const Main = () => {
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>СЕВЕРЯНОЧКА</h1>
+      <CardCatalog />
+      <Articles />
     </>
+    // <>
+    //   <h1 style={{ textAlign: "center" }}>СЕВЕРЯНОЧКА</h1>
+    // </>
   );
 };
