7ea1ebfb13b5b9019652aa82ffef9e68f2804655
I-D-D-I <3711080@gmail.com>
2023-03-17 19:37:45 +0500

    fix: main in Banners

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
 import { Layout } from "../../layout/Layout";
-import { Banners } from "../Banners/Banners";
 
 export const Main = () => {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>СЕВЕРЯНОЧКА</h1>
-      <Banners />
     </>
   );
 };
