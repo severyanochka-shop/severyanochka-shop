75b652ac936890d9fea113054b2874f7ff749a66
I-D-D-I <3711080@gmail.com>
2023-03-17 19:36:11 +0500

    feat: add Banners

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import { Layout } from "../../layout/Layout";
+import { Banners } from "../Banners/Banners";
 
 export const Main = () => {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>СЕВЕРЯНОЧКА</h1>
+      <Banners />
     </>
   );
 };
