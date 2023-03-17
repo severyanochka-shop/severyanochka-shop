c9b7d46aa8b3eb4d40b8c164b63454e90942c357
Margolex <margaritalexina@gmail.com>
2023-03-17 14:19:18 +0300

    delete from main

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,14 +1,11 @@
 import React from "react";
-import { FilterCategory } from "../../components/FilterCategory/FilterCategory";
-
 import { Layout } from "../../layout/Layout";
 
 
 export const Main = () => {
   return (
-    <>
-      <h1 style={{ textAlign: "center" }}>СЕВЕРЯНОЧКА</h1>
-      <FilterCategory/>
-    </>
+    
+  <h1 style={{ textAlign: "center" }}>СЕВЕРЯНОЧКА</h1>
+
   );
 };
