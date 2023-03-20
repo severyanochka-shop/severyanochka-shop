c02938f4804e40fb33ef77f6ffcd59c58424219b
Margolex <margaritalexina@gmail.com>
2023-03-20 18:50:03 +0300

    fix

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Basket } from "../../components/Basket/Basket";
 import { CategoryPage } from "../../components/CategoryPage/CategoryPage";
 import { FilterCategory } from "../../components/FilterCategory/FilterCategory";
 import { GridWrapper } from "../../layout/GridWrapper/GridWrapper";
@@ -7,7 +8,8 @@ export const Main = () => {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>СЕВЕРЯНОЧКА</h1>
-      <CategoryPage/>
+      <CategoryPage />
+      <Basket/>
     </>
   );
 };
