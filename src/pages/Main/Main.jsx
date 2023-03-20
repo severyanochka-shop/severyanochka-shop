f02d10b51eb863b99c900747c616d193f5827219
OrangeRootkit <katkovdv88@gmail.com>
2023-03-20 13:20:07 +0300

    fix:forms

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { AuthorizationForm } from "../../ui/Form/AuthorizationForm/AuthorizationForm"
-import { RegistrationFormSubmit } from "../../ui/Form/RegistrationFormSubmit/RegistrationFormSubmit";
-import { RegistrationForm } from "../../ui/Form/RegistrationForm/RegistrationForm";
+// import { AuthorizationForm } from "../../ui/Form/AuthorizationForm/AuthorizationForm"
+// import { RegistrationFormSubmit } from "../../ui/Form/RegistrationFormSubmit/RegistrationFormSubmit";
+// import { RegistrationForm } from "../../ui/Form/RegistrationForm/RegistrationForm";
 
 export const Main = () => {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>СЕВЕРЯНОЧКА</h1>
-      <AuthorizationForm />
+      {/* <AuthorizationForm /> */}
       {/* <RegistrationFormSubmit /> */}
       {/* <RegistrationForm /> */}
     </>
