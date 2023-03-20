36ede821c3187394b684c3bda4e0f5e62fac513b
Igor Karpov <karpov_igorspb@mail.ru>
2023-03-20 16:49:14 +0300

    refactor

---
+++ src/pages/BoughtBefore/BoughtBefore.async.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const BoughtBeforeAsync = React.lazy(() => import("./BoughtBefore"));
---
+++ src/pages/Stocks/Stocks.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "../../layout/Container/Container";
+
+export const Stocks = () => {
+  return (
+    <Container>
+      <h1>Акции</h1>
+    </Container>
+  );
+};
+
+export default Stocks;
