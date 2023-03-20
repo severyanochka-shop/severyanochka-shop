36f63299db47dd5ac75090928c3a152952db12cd
zimoscop <zimoscop@gmail.com>
2023-03-20 16:36:00 +0300

    fix:adapt

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -6,6 +6,7 @@ import { Breadcrumbs } from "../ui/Breadcrumbs/Breadcrumbs";
 import { Outlet } from "react-router-dom";
 import { HeaderMobileTop } from "./Header/HeaderMobile/HeaderMobileTop";
 import { HeaderMobileBottom } from "./Header/HeaderMobile/HeaderMobileBottom";
+import { ProductCard } from "../components/ProductCard/ProductCard";
 
 export const Layout = () => {
   return (
@@ -15,6 +16,7 @@ export const Layout = () => {
       <Breadcrumbs />
       <main className={s.main}>
         <Outlet />
+        <ProductCard />
       </main>
       <Footer />
       <HeaderMobileBottom />
