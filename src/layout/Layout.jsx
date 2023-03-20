017d6a055c473e0b6d19e8d0e9eed83376143402
zimoscop <zimoscop@gmail.com>
2023-03-20 13:19:15 +0300

    fix:tel

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,6 +4,7 @@ import { Footer } from "./Footer/Footer";
 import s from "./Layout.module.scss";
 import { Breadcrumbs } from "../ui/Breadcrumbs/Breadcrumbs";
 import { Outlet } from "react-router-dom";
+import { ContactPage } from "../components/ContactPage/ContactPage";
 
 export const Layout = () => {
   return (
@@ -11,6 +12,7 @@ export const Layout = () => {
       <Header />
       <Breadcrumbs />
       <main className={s.main}>
+        <ContactPage />
         <Outlet />
       </main>
       <Footer />
