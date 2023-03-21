11b23d0471ca70fca9855b173b7d47c05f61befe
OrangeRootkit <katkovdv88@gmail.com>
2023-03-21 15:50:44 +0300

    fix: forms

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,19 +5,10 @@ import { Section } from "../../layout/Section/Section";
 import { SectionLink } from "../../layout/SectionLink/SectionLink";
 import { SectionTitle } from "../../layout/SectionTitle/SectionTitle";
 import { SectionTitleWrapper } from "../../layout/SectionTitleWrapper/SectionTitleWrapper";
-import { AuthorizationForm } from "../../ui/Form/AuthorizationForm/AuthorizationForm";
-import { RegistrationForm } from "../../ui/Form/RegistrationForm/RegistrationForm";
-import { RegistrationFormSubmit } from "../../ui/Form/RegistrationFormSubmit/RegistrationFormSubmit";
-import { Textfield } from "../../ui/Textfield/Textfield";
 
 export const Main = () => {
   return (
     <>
-      <Textfield placeholder={"Найти товар"} header/>
-      <RegistrationForm />
-      <RegistrationFormSubmit />
-      <AuthorizationForm />
-
       <BannerPromo />
       <Section>
         <SectionTitleWrapper>
