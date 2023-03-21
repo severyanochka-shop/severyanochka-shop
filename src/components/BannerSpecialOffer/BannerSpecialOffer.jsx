9a7cdaf1d9e35047e5af1daa8218d0d8a6b4bfcc
I-D-D-I <3711080@gmail.com>
2023-03-21 15:29:10 +0500

    fix: BannerSpecialOffer scss

--- src/components/BannerSpecialOffer/BannerSpecialOffer.jsx
+++ src/components/BannerSpecialOffer/BannerSpecialOffer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import s from "./BannerSpecialOffer.module.scss";
-import cardofloyalty from "./images/cardofloyalty.png";
-import korzina from "./images/korzina.svg";
+import cardofloyalty from "./assets/cardofloyalty.png";
+import korzina from "./assets/korzina.svg";
 
 export const Banners = () => {
   return (
@@ -16,9 +16,9 @@ export const Banners = () => {
         <img src={cardofloyalty} alt="card" className={s.bannercard__img} />
       </div>
       <div className={s.bannerkorzina}>
-        <div className={s.bannerkorzina__texts}>
-          <h3 className={s.bannerkorzina__title}>Покупайте акционные товары</h3>
-          <p className={s.bannerkorzina__subtitle}>И получайте вдвое больше бонусов</p>
+        <div className={s.bannercard__texts}>
+          <h3 className={s.bannercard__title}>Покупайте акционные товары</h3>
+          <p className={s.bannercard__subtitle}>И получайте вдвое больше бонусов</p>
         </div>
         <img src={korzina} alt="card" className={s.bannerkorzina__img} />
       </div>
