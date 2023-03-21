0a8713b97cfacd7ce21a7376b7ce73e13bee6c62
I-D-D-I <3711080@gmail.com>
2023-03-21 19:17:34 +0500

    fix: AboutCompany component inside

--- src/components/AboutCompany/AboutCompany.jsx
+++ src/components/AboutCompany/AboutCompany.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Container } from "../../layout/Container/Container.jsx";
 import s from "./AboutCompany.module.scss";
 import groupPeople from "./assets/groupPeople.svg";
-import сhecksev from "./assets/checksev.svg";
+// import сhecksev from "./assets/checksev.svg";
 import logosevabout from "./assets/logosevabout.svg";
+import { AboutCompanyList } from "./AboutCompanyList/AboutCompanyList.jsx";
 
 export const AboutCompany = () => {
   return (
@@ -12,37 +13,22 @@ export const AboutCompany = () => {
         <div className={s.about__head}>
           <div className={s.about__titles}>
             <h3 className={s.about__subtitle}>
-              Мы непрерывно развиваемся и <nbsp />
-              <br className={s.about__subtitle_br} />
-              работаем над совершенствованием сервиса, <nbsp />
-              <br className={s.about__subtitle_br} />
-              заботимся о наших клиентах, <nbsp />
-              <br className={s.about__subtitle_br} />
-              стремимся к лучшему будущему.
+              Мы непрерывно развиваемся и работаем над совершенствованием сервиса, заботимся о наших
+              клиентах, стремимся к лучшему будущему.
             </h3>
           </div>
           <img src={groupPeople} alt="groupPeople" className={s.about__img} />
         </div>
         <div className={s.about__checkboxes}>
-          <div className={s.about__texts}>
-            <img src={сhecksev} alt="сheck" className={s.about__check_img} />
-            <p className={s.about__textOne}>Мы занимаемся розничной торговлей</p>
-            <p className={s.about__textTwo}>Более 20 лет.</p>
-          </div>
-          <div className={`${s.about__texts} ${s.about__textssecond}`}>
-            <img src={сhecksev} alt="сheck" className={s.about__check_img} />
-            <p className={s.about__textOne}>Основная миссия компании</p>
-            <p className={s.about__textTwo}>
-              Максимальное качество товаров и услуг по доступной цене.
-            </p>
-          </div>
-          <div className={`${s.about__texts} ${s.about__textsthree}`}>
-            <img src={сhecksev} alt="сheck" className={s.about__check_img} />
-            <p className={s.about__textOne}>Отличительная черта нашей сети</p>
-            <p className={s.about__textTwo}>
-              Здоровая и полезная продукция местного производства в наших магазинах.
-            </p>
-          </div>
+          <AboutCompanyList text1="Мы занимаемся розничной торговлей" text2="Более 20 лет." />
+          <AboutCompanyList
+            text1="Основная миссия компании"
+            text2="Максимальное качество товаров и услуг по доступной цене."
+          />
+          <AboutCompanyList
+            text1="Отличительная черта нашей сети"
+            text2="Здоровая и полезная продукция местного производства в наших магазинах."
+          />
         </div>
         <div className={s.about__thanks}>
           <img src={logosevabout} alt="logo" className={s.about__thanks_img} />
