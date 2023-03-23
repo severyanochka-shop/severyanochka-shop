8a3acad1bac488765ccb3f1fa94785ec5a4976f4
Andrei Beliakov <andrei@imd.ru>
2023-03-23 18:15:53 +0300

    feat: made dropdown menu on catalog button hover

--- src/layout/Header/HeaderMobile/HeaderMobileTop.jsx
+++ src/layout/Header/HeaderMobile/HeaderMobileTop.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import s from "./HeaderMobileTop.module.scss";
-import logo_mobile from "../images/logos/logo_mobile.svg";
+import logo_mobile from "../assets/logos/logo_mobile.svg";
 import { Container } from "../../Container/Container";
 import { Textfield } from "../../../ui/Textfield/Textfield";
 
