43c475bc09651bf2fb6096f1afacab7f7df4f0d2
Andrei Beliakov <andrei@imd.ru>
2023-03-23 13:10:09 +0300

    fix: fixed header adaptives

--- src/layout/Header/HeaderButton/HeaderButton.jsx
+++ src/layout/Header/HeaderButton/HeaderButton.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import clsx from "clsx";
 import s from "./HeaderButton.module.scss";
 
-export const HeaderButton = ({ className, children, background, handler, size }) => {
+export const HeaderButton = ({ className, children, background, handler }) => {
   const innerClassName = clsx(s.button, className, {
-    [s.button__large]: size === "large",
     [s.button__green]: background === "green",
     [s.button__orange]: background === "orange",
   });
