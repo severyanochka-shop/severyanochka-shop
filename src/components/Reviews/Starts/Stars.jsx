986dc873958282147723af11e25bf3ba3d071831
zimoscop <zimoscop@gmail.com>
2023-03-24 12:29:27 +0300

    fix:stars

--- src/components/Reviews/Starts/Stars.jsx
+++ src/components/Reviews/Starts/Stars.jsx
@@ -6,11 +6,9 @@ import star from "../Images/starfull.svg";
 export const Stars = ({ type }) => {
   return (
     <div className={s.score_stars}>
-      {type >= 1 ? <img src={star} alt="★" /> : <img src={emptystar} alt="☆" />}
-      {type >= 2 ? <img src={star} alt="★" /> : <img src={emptystar} alt="☆" />}
-      {type >= 3 ? <img src={star} alt="★" /> : <img src={emptystar} alt="☆" />}
-      {type >= 4 ? <img src={star} alt="★" /> : <img src={emptystar} alt="☆" />}
-      {type >= 5 ? <img src={star} alt="★" /> : <img src={emptystar} alt="☆" />}
+      {[1, 2, 3, 4, 5].map((el) => (
+        <img src={type >= el ? star : emptystar} alt={type >= el ? "★" : "☆"} />
+      ))}
     </div>
   );
 };
