a2040c62f7bf322fd675dd3e5d7c1d9dc6a42201
OrangeRootkit <katkovdv88@gmail.com>
2023-03-24 18:01:42 +0300

    fix:politics

--- src/components/PersonalData/PersonalData.jsx
+++ src/components/PersonalData/PersonalData.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import s from "./PersonalData.module.scss";
-import data from "./data.json"
-import { useEffect, useState } from "react";
-import { Title } from "../../layout/Title/Title";
-import politics from "./politics.json"
+import data from "./data.json";
 
 export const PersonalData = () => {
-
-
   let arr = Object.entries(data);
-  
 
   return (
     <div className={s.wrapper}>
       <ul className={s.list}>
-      {arr.map((el, index)=><li key={index}>{el[1]}</li>)}
-
+        {arr.map((el, index) => (
+          <li key={index}>{el[1]}</li>
+        ))}
       </ul>
     </div>
   );
