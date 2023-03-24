7900b953dd9896262515ab117a02ae23e96a486c
Andrei Beliakov <andrei@imd.ru>
2023-03-24 14:12:34 +0300

    fix assets

--- src/components/BannerPromo/BannerPromo.jsx
+++ src/components/BannerPromo/BannerPromo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import s from "./BannerPromo.module.scss";
-import basket_big from "./images/basket_big.png";
-import basket_small from "./images/basket_small.png";
+import basket_big from "./assets/basket_big.png";
+import basket_small from "./assets/basket_small.png";
 
 export const BannerPromo = () => {
   return (
@@ -10,7 +10,7 @@ export const BannerPromo = () => {
       <picture>
         <source media="(max-width: 1207px)" srcset={basket_small} />
         <source media="(min-width: 1208px)" srcset={basket_big} />
-        <img src={basket_big} alt="" className={s.wrapper__image} />
+        <img src={basket_big} alt="Basket" className={s.wrapper__image} />
       </picture>
       <h1 className={s.wrapper__title}>Доставка бесплатно от 1000 ₽</h1>
     </div>
