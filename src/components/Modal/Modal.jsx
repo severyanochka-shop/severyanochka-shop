f6b8505d9207c84764ff45e4ade64d2e5ac21aad
zimoscop <zimoscop@gmail.com>
2023-03-24 16:52:47 +0300

    fix:scroll

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import s from "./Modal.module.scss";
+import { useLayoutEffect } from "react";
 
 export const Modal = ({ children }) => {
+  useLayoutEffect(() => {
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+    document.body.style.overflow = "hidden";
+    return () => (document.body.style.overflow = originalStyle);
+  }, []);
+
   return (
     <div
       className={s.modal}
