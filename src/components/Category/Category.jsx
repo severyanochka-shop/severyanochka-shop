c5261cbc5b1da1237501095d0d7691411fa78810
Igor Karpov <karpov_igorspb@mail.ru>
2023-03-28 17:30:15 +0300

    feat: fetch categories from api

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import { CategoryItem } from "./CategoryItem/CategoryItem";
 import s from "./Category.module.scss";
-import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
-export const Category = () => {
-  const { categories } = useSelector((state) => state.categoriesReducer);
+export const Category = ({ categories }) => {
   return (
     <div className={s.category}>
       {categories.map((el) => (
-        <Link key={el.id} to={`${el.slug}`} className={s.link}>
+        <Link key={el.id} to={`${el.slug}`} className={s.link} onClick={()=>{}}>
           <CategoryItem name={el.name} image={el.image} />
         </Link>
       ))}
