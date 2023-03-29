767026566c8d479996e93f18f3aa2e75ba9feeb9
zimoscop <zimoscop@gmail.com>
2023-03-29 17:11:24 +0300

    feat:ourshops

---
+++ src/components/OurShops/OurShops.jsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import s from "./OurShops.module.scss";
+import voshod from "./assets/voshod.svg";
+import parus from "./assets/parus.svg";
+import rjaba from "./assets/rjabina.svg";
+import los from "./assets/pellis.svg";
+import { ContactItem } from "../ContactItem/ContactItem";
+import { YMaps } from "@pbe/react-yandex-maps";
+import { YandexMaps } from "../YandexMaps/YandexMaps";
+import { useLocation } from "react-router-dom";
+
+export const OurShops = () => {
+  const [active, setActive] = useState("shel");
+  const { pathname } = useLocation();
+
+  useEffect(() => {}, [pathname]);
+
+  const handleClick = (e) => {
+    setActive(e.target.id);
+  };
+
+  const background = (id) => {
+    return { backgroundColor: active === id ? "#70c05b" : "rgba(243, 242, 241, 1)" };
+  };
+
+  const shops = [
+    {
+      name: "п.Щельяюр",
+      id: "shel",
+      adress: "ул.Дорожная",
+      phone: "+79042713590",
+      img: { src: voshod, alt: "Восход" },
+    },
+    {
+      name: "д.Вертеп",
+      id: "vert",
+      adress: "ул. Советская 87",
+      phone: "+7 8214091330",
+      img: { src: parus, alt: "Парус" },
+    },
+    {
+      name: "с.Краснобор",
+      id: "kras",
+      adress: "ул. Заводская 16",
+      phone: "+7 8214091101",
+      img: { src: rjaba, alt: "Рябинушка" },
+    },
+    {
+      name: "д.Диюр",
+      id: "diur",
+      adress: "ул. Рабочая 1",
+      phone: "+7 8214091300",
+      img: { src: los, alt: "Пелысь" },
+    },
+  ];
+  return (
+    <div className={s.ourshops}>
+      <h3 className={s.ourshops__subtitle}>Наши магазины</h3>
+      <div className={s.item2}>
+        {shops.map((el, i) => (
+          <>
+            <button
+              key={i}
+              id={el.id}
+              onClick={handleClick}
+              className={s.location__btn}
+              style={background(el.id)}
+            >
+              {el.name}
+            </button>
+          </>
+        ))}
+      </div>
+      <div className={s.item3}>
+        {shops.map((el, i) => (
+          <ContactItem
+            key={i.toString()}
+            adress={el.adress}
+            phone={el.phone}
+            img={el.img.src}
+            alt={el.img.alt}
+          />
+        ))}
+      </div>
+      <YMaps>
+        <YandexMaps width="1208px" height="354px" />
+      </YMaps>
+    </div>
+  );
+};
