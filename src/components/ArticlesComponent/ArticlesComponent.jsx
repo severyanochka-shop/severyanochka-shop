1b6d273a6f62bba702f76985b7ee2c3ff5460db5
Andrei Beliakov <andrei@imd.ru>
2023-03-29 18:14:46 +0300

    fix: Articles page style

--- src/components/ArticlesComponent/ArticlesComponent.jsx
+++ src/components/ArticlesComponent/ArticlesComponent.jsx
@@ -6,10 +6,14 @@ import s from "./ArticlesComponent.module.scss";
 export const ArticlesComponent = ({ date, title, text, image }) => {
   return (
     <Container>
-      <span className={s.article__date}>{date}</span>
-      <img src={image} alt={title} className={s.article__image} />
-      <h1 className={s.article__title}>{title}</h1>
-      <p className={s.article__text}>{text}</p>
+      <div className={s.article}>
+        <img src={image} alt={title} className={s.article__image} />
+        <div className={s.article__wrapper}>
+          <span className={s.article__date}>{date}</span>
+          <h1 className={s.article__title}>{title}</h1>
+          <p className={s.article__text}>{text}</p>
+        </div>
+      </div>
     </Container>
   );
 };
